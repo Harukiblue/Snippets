743d973580a9b561587bbe3d6421dbbdf44eff63
Brennien Coker <bcoker@rjf.com>
2020-06-05 14:24:13 -0400

    remove unrelated code

--- [JS] Event listener (multiple browsers).js
+++ [JS] Event listener (multiple browsers).js
@@ -5,20 +5,3 @@ function addEvent(el, evnt, funct){
   }
   return el.addEventListener(evnt, funct, false);
 }
-
-
-
-function recreateNode(el, withChildren) {
-  if (withChildren) {
-    el.parentNode.replaceChild(el.cloneNode(true), el);
-  }else{
-    var newEl = el.cloneNode(false);
-    while (el.hasChildNodes()) newEl.appendChild(el.firstChild);
-    el.parentNode.replaceChild(newEl, el);
-  }
-}
-
-//Remove event listeners on one element:
-recreateNode(document.getElementById("btn"));
-//Remove event listeners on an element and all of its children:
-recreateNode(document.getElementById("list"), true);
