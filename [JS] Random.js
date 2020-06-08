87f35b85dd2b9be1e170eb9e9374cf5d4abec068
Brennien Coker <bcoker@rjf.com>
2020-06-08 13:41:01 -0400

    added int and decimal versions

--- [JS] Random.js
+++ [JS] Random.js
@@ -1,5 +1,22 @@
-function GetRandomNumber(max, min){
+
+function GetRandomInt(max, min){
     max = max === undefined ? 0 : max;
     min = min === undefined ? 0 : min;
+    if(max < min){
+        var tmax = max;
+        max = min;
+        min = tmax;
+    }
     return Math.floor(Math.random() * ((max - min) + 1)) + min; 
+}
+
+function GetRandomDecimal(max, min){
+    if(max < min){
+        var tmax = max;
+        max = min;
+        min = tmax;
+    }
+    max = max === undefined ? 0 : max;
+    min = min === undefined ? 0 : min;
+    return Math.random() * (max - min) + min; 
 }
