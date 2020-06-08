84b0a144392a74748d4e01807f13347087bd24c5
Brennien Coker <bcoker@rjf.com>
2020-06-08 11:05:43 -0400

    refactorings

--- [JS] Validate Curency.js
+++ [JS] Validate Curency.js
@@ -1,34 +1,40 @@
 /**
 * Currency
 **/
-var currencies = document.getElementsByClassName("currency");
-for(var i = 0; i < currencies.length; i++){
-	//default Value
-	currencies[i].value = (currencies[i].value === "" || currencies[i].value === null || currencies[i].value === undefined) ? "0.00" : currencies[i].value;
-	//On Focus
-	currencies[i].addEventListener("focus", function(e){
-		var target = e.srcElement || e.target;
-		var val = target.value;
-		var RTN = convertToFloat(val);
-		target.value = RTN === 0 ? "" : RTN;
-	});
-	//On Blur
-	currencies[i].addEventListener("blur", function(e){
-		var target = e.srcElement || e.target;
-		var val = target.value;
-		target.value = covertToCurrency(val);
-	});
+function Currency(value){
+	this.value = covertToCurrency(value);
 }
-function covertToCurrency(val){
-	var RTN = "" + convertToFloat(val);
+
+Currency.prototype.convertToFloat = function(value){
+	var RTN = parseFloat(value.replace(/[,|$]/g,""));
+	return isNaN(RTN) ? 0.00: RTN;
+}
+Currency.prototype.covertToCurrency = function(value){
+	var RTN = "" + convertToFloat(value);
 	RTN = RTN.replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, "$1,")
 	return RTN.indexOf(".") == -1 ? RTN + ".00" : RTN;
 }
-function convertToFloat(val){
-	var RTN = parseFloat(val.replace(/[,|$]/g,""));
-	return isNaN(RTN) ? 0.00: RTN;
+Currency.prototype.Validate = function(value){
+	return /(?=.)^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$/.test(value);
 }
-function validateCurrency(elementValue){
-	var currencyPattern = /(?=.)^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$/;
-	return currencyPattern.test(elementValue);
+Currency.prototype.AddCurrencies = function(){
+	var ctx = this;
+	var currencies = document.getElementsByClassName("currency");
+	for(var i = 0; i < currencies.length; i++){
+		//default Value
+		currencies[i].value = (currencies[i].value === "" || currencies[i].value === null || currencies[i].value === undefined) ? "0.00" : currencies[i].value;
+		//On Focus
+		currencies[i].addEventListener("focus", function(e){
+			var target = e.srcElement || e.target;
+			var val = target.value;
+			var RTN = ctx.convertToFloat(val);
+			target.value = RTN === 0 ? "" : RTN;
+		});
+		//On Blur
+		currencies[i].addEventListener("blur", function(e){
+			var target = e.srcElement || e.target;
+			var val = target.value;
+			target.value = ctx.covertToCurrency(val);
+		});
+	}
 }
--- [JS] Validate Phone.js
+++ [JS] Validate Phone.js
@@ -1,30 +1,43 @@
 
 function Phone(value){
-	var convert = function(value){
-		if(value === undefined || value === null || value === ""){return null;}
-		var original = value;
-		value = value.replace(/\s/g,"").replace(/\(/g,"").replace(/\)/g,"").replace(/\./g,"").replace(/-/g,"");
-		if(value.length > 3 && value.length < 12){
-			var pattern;
-			if(value.length > 3 && value.length < 8){
-				pattern = "xxx-xxxx";
-			}else if(value.length > 7 && value.length < 11){
-				pattern = "(xxx) xxx-xxxx";
-			}else{
-				pattern = "x(xxx) xxx-xxxx";
-			}
-			for(var i = 0; i < value.length; i++){
-				pattern = pattern.replace("x",value.substring(i,i+1));
-			}
-			pattern = pattern.replace(/x/g,"");
-			return pattern;
-		}else{
-			return original;
-		}
-	};
-
-	this.value = convert(value);
+	if(value !== undefined) this.value = this.convertToPhone(value);
 }
+Phone.prototype.convertToPhone = function(value){
+    if(value === undefined || value === null || value === ""){return null;}
+    var original = value;
+    value = value.replace(/\s/g,"").replace(/\(/g,"").replace(/\)/g,"").replace(/\./g,"").replace(/-/g,"");
+    if(value.length > 3 && value.length < 12){
+        var pattern;
+        if(value.length > 3 && value.length < 8){
+            pattern = "xxx-xxxx";
+        }else if(value.length > 7 && value.length < 11){
+            pattern = "(xxx) xxx-xxxx";
+        }else{
+            pattern = "x(xxx) xxx-xxxx";
+        }
+        for(var i = 0; i < value.length; i++){
+            pattern = pattern.replace("x",value.substring(i,i+1));
+        }
+        pattern = pattern.replace(/x/g,"");
+        return pattern;
+    }else{
+        return original;
+    }
+};
 Phone.prototype.Validate = function(){
-	return /^[0-9]?[ |\.]?[\(]?[0-9]{3}[\)]?[ |\-|\.]?[0-9]{3}[-|\.]?[0-9]{4}$/.test(this.value)
+    return /^[0-9]?[ |\.]?[\(]?[0-9]{3}[\)]?[ |\-|\.]?[0-9]{3}[-|\.]?[0-9]{4}$/.test(this.value)
+}
+Phone.prototype.AddPhones = function(className){
+	var ctx = this;
+	className = (className === undefined || className === null || className === "") ? "currency" : className;
+	var phones = document.getElementsByClassName(className);
+	for(var i = 0; i < phones.length; i++){
+		phone = phones[i];
+		phone.addEventListener("change", function(e){
+			var target = e.srcElement || e.target;
+			var value = target.value;
+			var result = ctx.convertToPhone(value);
+			target.value = result === 0 ? "" : result;
+		});
+    }
 }
